acae199da35fc54183940f72cc52513495579547
pavan sai <rebbavenkatapavansai@gmail.com>
2024-09-08 21:11:02 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
+import amma1 from './amma1.jpeg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={amma1} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         good morning
         </p>
         <a
           className="App-link"
---
+++ src/CLS.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class cls extends Component {
+  render() {
+    return (
+      <div><p>iam from rcc</p></div>
+    )
+  }
+}
